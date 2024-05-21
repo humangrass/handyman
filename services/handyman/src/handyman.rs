c0412b8f2d3d2eea60271e4431dec0657eeacbdf
Alex Mikhankov <jesslich.nime@gmail.com>
2024-05-21 22:58:50 +0300

    refactoring: add_jobs macro

--- services/handyman/src/handyman.rs
+++ services/handyman/src/handyman.rs
@@ -9,6 +9,25 @@ use jupiter::JupiterExecutor;
 use crate::yaml::tasks::{Tasks};
 
 
+// It would be better to use traits instead :D
+macro_rules! add_jobs {
+    ($self:ident, $scheduler:ident, $tasks:expr, $executor:ty) => {
+        for task in $tasks.iter() {
+            let repo = $self.repo.clone();
+            let executor = <$executor>::new(repo, task.clone().task());
+            let job = Job::new(task.cron.to_string().parse().unwrap(), move || {
+                let executor = executor.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = executor.execute().await {
+                        error!("Error executing task: {:?}\n{:?}", executor.task, e);
+                    }
+                });
+            });
+            $scheduler.add(job);
+        }
+    };
+}
+
 pub struct Handyman {
     pub repo: Repository,
     pub tasks: Tasks,
@@ -23,31 +42,8 @@ impl Handyman {
     pub async fn run(&self) {
         let mut scheduler = JobScheduler::new();
 
-        for task in self.tasks.bitget.iter() {
-            let repo = self.repo.clone();
-            let executor = BitgetExecutor::new(repo, task.clone().task());
-            let job = Job::new(task.cron.to_string().parse().unwrap(), move || {
-                let executor = executor.clone();
-                tokio::spawn(async move {
-                    if let Err(e) = executor.execute().await {
-                        error!("Error executing task: {:?}\n{:?}", executor.task, e);
-                    }
-                });
-            });
-            scheduler.add(job);
-        }
-
-        for task in self.tasks.jupiter.iter() {
-            let repo = self.repo.clone();
-            let executor = JupiterExecutor::new(repo, task.clone().task());
-            let job = Job::new(task.cron.to_string().parse().unwrap(), move || {
-                let executor = executor.clone();
-                tokio::spawn(async move {
-                    executor.execute().await
-                });
-            });
-            scheduler.add(job);
-        }
+        add_jobs!(self, scheduler, self.tasks.bitget, BitgetExecutor);
+        add_jobs!(self, scheduler, self.tasks.jupiter, JupiterExecutor);
 
         loop {
             scheduler.tick();
